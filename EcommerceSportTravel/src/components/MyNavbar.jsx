185cf088ee8b5a70306843467e606f7e5229e597
Inchino <alessandro.incalza22@gmail.com>
2025-05-05 16:51:13 +0200

    Navbar migliorata

--- EcommerceSportTravel/src/components/MyNavbar.jsx
+++ EcommerceSportTravel/src/components/MyNavbar.jsx
@@ -1,7 +1,8 @@
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown, Image } from "react-bootstrap";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../redux/authSlice";
+import { FaUserCircle } from "react-icons/fa";
 import "./MyNavbar.css";
 
 function MyNavBar() {
@@ -15,26 +16,46 @@ function MyNavBar() {
   };
 
   return (
-    <Navbar expand="lg" className="bg-custom" variant="dark">
+    <Navbar expand="lg" className="bg-custom shadow-sm" variant="dark">
       <Container>
-        <Navbar.Brand as={NavLink} to="/" className="brand-custom">
-          ⚽ Calcio e Viaggi
+        <Navbar.Brand as={NavLink} to="/" className="brand-custom d-flex align-items-center">
+          <Image src="/GoalAwayLogo.png" alt="GoalAway logo" height="55" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light" />
-        <Navbar.Collapse id="basic-navbar-nav">
+
+        <Navbar.Toggle aria-controls="navbar-content" className="border-0" />
+
+        <Navbar.Collapse id="navbar-content">
           <Nav className="ms-auto">
-            <Nav.Link as={NavLink} to="/" className="link-custom">
+            <Nav.Link
+              as={NavLink}
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "link-custom active-link" : "link-custom"
+              }
+            >
               Home
             </Nav.Link>
 
-            <Nav.Link as={NavLink} to="/packages" className="link-custom">
+            <Nav.Link
+              as={NavLink}
+              to="/packages"
+              className={({ isActive }) =>
+                isActive ? "link-custom active-link" : "link-custom"
+              }
+            >
               Pacchetti Viaggio
             </Nav.Link>
 
             {user ? (
               <>
                 {user.role === "Admin" && (
-                  <Nav.Link as={NavLink} to="/admin" className="link-custom">
+                  <Nav.Link
+                    as={NavLink}
+                    to="/admin"
+                    className={({ isActive }) =>
+                      isActive ? "link-custom active-link" : "link-custom"
+                    }
+                  >
                     Admin
                   </Nav.Link>
                 )}
@@ -42,15 +63,23 @@ function MyNavBar() {
                 <Nav.Link
                   as={NavLink}
                   to="/userBookings"
-                  className="link-custom"
+                  className={({ isActive }) =>
+                    isActive ? "link-custom active-link" : "link-custom"
+                  }
                 >
                   Le mie prenotazioni
                 </Nav.Link>
 
                 <NavDropdown
-                  title={user.name}
+                  title={
+                    <span className="d-flex align-items-center user-dropdown-title">
+                      <FaUserCircle className="me-1" size={18} />
+                      {user.name}
+                    </span>
+                  }
                   id="user-dropdown"
-                  className="link-custom"
+                  align="end"
+                  className="dropdown-custom"
                 >
                   <NavDropdown.Item onClick={handleLogout}>
                     Logout
@@ -59,10 +88,22 @@ function MyNavBar() {
               </>
             ) : (
               <>
-                <Nav.Link as={NavLink} to="/login" className="link-custom">
+                <Nav.Link
+                  as={NavLink}
+                  to="/login"
+                  className={({ isActive }) =>
+                    isActive ? "link-custom active-link" : "link-custom"
+                  }
+                >
                   Login
                 </Nav.Link>
-                <Nav.Link as={NavLink} to="/signup" className="link-custom">
+                <Nav.Link
+                  as={NavLink}
+                  to="/signup"
+                  className={({ isActive }) =>
+                    isActive ? "link-custom active-link" : "link-custom"
+                  }
+                >
                   Registrati
                 </Nav.Link>
               </>
